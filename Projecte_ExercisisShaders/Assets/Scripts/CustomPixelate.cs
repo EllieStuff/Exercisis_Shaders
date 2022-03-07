a3fd01c12a7dab3fc7227f3392ac064bebb3654d
Mílion <joel.mateu@enti.cat>
2022-03-07 11:05:34 +0100

    David bi(s) square

--- Projecte_ExercisisShaders/Assets/Scripts/CustomPixelate.cs
+++ Projecte_ExercisisShaders/Assets/Scripts/CustomPixelate.cs
@@ -7,26 +7,29 @@ using UnityEngine.Rendering.PostProcessing;
 //Needed to let unity serialize this and extend PostProcessEffectSettings
 [Serializable]
 //Using [PostProcess()] attrib allows us to tell Unity that the class holds postproccessing data. 
-[PostProcess(renderer: typeof(CustomPostproInvertColors),//First parameter links settings with actual renderer
+[PostProcess(renderer: typeof(CustomPostproPixelate),//First parameter links settings with actual renderer
             PostProcessEvent.AfterStack,//Tells Unity when to execute this postpro in the stack
-            "Custom/InvertColors")] //Creates a menu entry for the effect
+            "Custom/Pixelate")] //Creates a menu entry for the effect
                                     //Forth parameter that allows to decide if the effect should be shown in scene view
-public sealed class InvertColorsSettings : PostProcessEffectSettings
+public sealed class PixelateSettings : PostProcessEffectSettings
 {
-    [Range(0f, 1f), Tooltip("Effect Intensity.")]
-    public FloatParameter blend = new FloatParameter { value = 0.0f }; //Custom parameter class, full list at: /PostProcessing/Runtime/
-                                                                       //The default value is important, since is the one that will be used for blending if only 1 of volume has this effect
+    [Range(1f, 500f), Tooltip("Effect Resolution.")]
+    public FloatParameter resolution = new FloatParameter { value = 0.0f };
+
+    [Range(1f, 10f), Tooltip("Effect Pixel Size.")]
+    public FloatParameter pixelSize = new FloatParameter { value = 0.0f };
 }
 
-public class CustomPostproInvertColors : PostProcessEffectRenderer<InvertColorsSettings>//<T> is the setting type
+public class CustomPostproPixelate : PostProcessEffectRenderer<PixelateSettings>//<T> is the setting type
 {
     public override void Render(PostProcessRenderContext context)
     {
         //We get the actual shader property sheet
-        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/InvertColors"));
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Pixelate"));
 
         //Set the uniform value for our shader
-        sheet.properties.SetFloat("_intensity", settings.blend);
+        sheet.properties.SetFloat("_resolution", settings.resolution);
+        sheet.properties.SetFloat("_targetPixelSize", settings.pixelSize);
 
         //We render the scene as a full screen triangle applying the specified shader
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
